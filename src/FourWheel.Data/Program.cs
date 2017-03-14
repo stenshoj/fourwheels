dddfa4ef3258e56d3bc5b29af175fa210ed22b9c
Martin Hansen <mart303m@edu.mercantec.dk>
2017-03-14 10:49:09 +0100

    Created template

---
+++ src/FourWheel.Data/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FourWheel.Data
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
--- src/FourWheel.Web/Startup.cs
+++ src/FourWheel.Web/Startup.cs
@@ -16,6 +16,7 @@ namespace FourWheel.Web
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,12 +27,11 @@ namespace FourWheel.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
